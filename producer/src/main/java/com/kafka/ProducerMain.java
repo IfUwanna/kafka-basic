ee3da48de85cb5487a157f830fd554cb5d401124
박지훈 <jihun.park8@cj.net>
2022-04-06 15:11:50 +0900

    220406-Producer

--- producer/src/main/java/com/kafka/ProducerMain.java
+++ producer/src/main/java/com/kafka/ProducerMain.java
@@ -25,26 +25,32 @@ public class ProducerMain {
         try {
             // set kafka properties
             Properties configs = new Properties();
-            configs.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
-            configs.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
-            configs.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+            configs.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");  // kafka cluster
+            configs.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());// KEY_SERIALIZER
+            configs.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName()); // VALUE_SERIALIZER
 
             // init KafkaProducer
             KafkaProducer<String, String> producer = new KafkaProducer<>(configs);
 
             int idx = 0;
             while(true){
+
                 // set ProducerRecord
                 String topic = "test";           // topic name
-                String data = "record-"+ idx++;  // data
-                ProducerRecord<String, String> record = new ProducerRecord<>(topic, data);
+                Integer partition = 0;               // partition number
+                String key = "key-" + idx;       // key
+                String data = "record-"+ idx;    // data
+                ProducerRecord<String, String> record = new ProducerRecord<>(topic, data);                  // ProducerRecord(@NotNull String topic, V value)
+                ProducerRecord<String, String> record2 = new ProducerRecord<>(topic, key, data);            // ProducerRecord(@NotNull String topic, K key, V value)
+                ProducerRecord<String, String> record3 = new ProducerRecord<>(topic, partition, key, data); // ProducerRecord(@NotNull String topic, Integer partition, K key, V value)
 
                 // send record (per second)
                 producer.send(record);
+
                 System.out.println("producer.send() >> [topic:" + topic + "][data:" + data + "]");
                 Thread.sleep(1000);
+                idx++;
             }
-
         } catch (Exception e) {
             System.out.println(e);
         }
