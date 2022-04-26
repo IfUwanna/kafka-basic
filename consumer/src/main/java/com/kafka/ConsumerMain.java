4758c1837d4e6b3c1525509472147b2675fcfcc5
박지훈 <jihun.park8@cj.net>
2022-04-26 15:46:38 +0900

    220427-consumer

--- consumer/src/main/java/com/kafka/ConsumerMain.java
+++ consumer/src/main/java/com/kafka/ConsumerMain.java
@@ -1,11 +1,17 @@
 package com.kafka;
 
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
 import org.apache.kafka.clients.consumer.KafkaConsumer;
 import org.apache.kafka.clients.producer.KafkaProducer;
 import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.clients.producer.ProducerRecord;
 import org.apache.kafka.common.serialization.StringSerializer;
 
+import java.time.Duration;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Properties;
 
 /**
@@ -25,16 +31,33 @@ public class ConsumerMain {
 
         try {
             // set kafka properties
-            Properties configs = new Properties();
-            configs.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");  // kafka cluster
-            configs.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());// KEY_SERIALIZER
-            configs.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName()); // VALUE_SERIALIZER
+            Properties props = new Properties();
+            props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");  // kafka cluster
+            props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());// KEY_SERIALIZER
+            props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName()); // VALUE_SERIALIZER
+            //props.setProperty("group.id", "test");
+            //props.setProperty("enable.auto.commit", "false");
 
             // init KafkaConsumer
-            KafkaConsumer<String, String> consumer = new KafkaConsumer<>(configs);
+            KafkaConsumer<String, String> consumer = new KafkaConsumer<>(props);
 
+            consumer.subscribe(Arrays.asList("foo", "bar"));
+            final int minBatchSize = 200;
+            List<ConsumerRecord<String, String>> buffer = new ArrayList<>();
+            while (true) {
+                ConsumerRecords<String, String> records = consumer.poll(Duration.ofMillis(100));
+                for (ConsumerRecord<String, String> record : records) {
+                    buffer.add(record);
+                }
+                if (buffer.size() >= minBatchSize) {
+                    System.out.println(buffer);
+                    consumer.commitSync();
+                    buffer.clear();
+                }
+            }
         } catch (Exception e) {
             System.out.println(e);
         }
+
     }
 }
